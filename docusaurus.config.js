a7e71cb6d34579c5a01584903c69c900d6b379e0
Anna Skoulikari <annaskoulikari@gmail.com>
2023-10-19 12:19:42 +0200

    Fixed url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://annaskoulikari.github.io/gitit",
+  url: "https://annaskoulikari.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
